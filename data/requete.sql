66e97f4c798cd72f8734384fe3bd816c006e7488
Quentinc63 <layk63@gmail.com>
2025-03-19 16:08:32 +0100

    feat: add a requete for getting nutritionnal value for a day

--- data/requete.sql
+++ data/requete.sql
@@ -15,3 +15,15 @@ JOIN food_meal fm ON m.id = fm.meal_id
 JOIN food f ON f.id = fm.food_id
 WHERE m.id = 2
 GROUP BY m.id
+
+-- Avoir le total de nos apport nutriotionnel sur une journée 
+
+SELECT d.id,d.date, SUM(f.calories * fm.quantity_g) AS calories_meal , SUM(f.protein * fm.quantity_g) AS protein_meal, SUM(f.fat * fm.quantity_g) AS fat_meal,
+ SUM(f.carbs * fm.quantity_g) AS carbs_meal
+FROM `day` d 
+JOIN day_meal dm ON d.id = dm.day_id
+JOIN `meal` m ON dm.meal_id = m.id
+JOIN food_meal fm ON m.id = fm.meal_id
+JOIN food f ON f.id = fm.food_id
+WHERE d.id = 2
+GROUP BY d.id
